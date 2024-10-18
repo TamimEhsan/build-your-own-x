7a44935cf85999679961f8d6635278eca34a277e
Tamim Ehsan <tamimehsan99@gmail.com>
2024-10-18 12:37:12 +0600

    fixed init git issue

--- git/main.go
+++ git/main.go
@@ -100,13 +100,41 @@ func git_init(directory string) {
 	// create a .git directory
 	createDir(path.Join(directory, ".git"))
 	// create subdirectories
-	for _, dir := range []string{"branches", "hooks", "info", "logs", "objects", "refs"} {
+	for _, dir := range []string{"branches", "hooks", "info", "objects", "refs"} {
 		createDir(path.Join(directory, ".git", dir))
 	}
 	// create files
-	for _, file := range []string{"HEAD", "config", "description", "index", "packed-refs"} {
+	for _, file := range []string{"HEAD", "config", "description" /*, "index", "packed-refs"*/} {
 		createFile(path.Join(directory, ".git", file))
 	}
+
+	// create objects subdirectories
+	for _, dir := range []string{"info", "pack"} {
+		createDir(path.Join(directory, ".git", "objects", dir))
+	}
+
+	// create refs subdirectories
+	for _, dir := range []string{"heads", "tags"} {
+		createDir(path.Join(directory, ".git", "refs", dir))
+	}
+
+	// create refs subdirectories
+	createFile(path.Join(directory, ".git", "info", "exclude"))
+
+	// write to HEAD file
+	headFile, _ := os.OpenFile(path.Join(".git", "HEAD"), os.O_RDWR|os.O_CREATE, 0644)
+	defer headFile.Close()
+	headFile.Write([]byte("ref: refs/heads/master\n"))
+
+	// write to config file
+	configFile, _ := os.OpenFile(path.Join(".git", "config"), os.O_RDWR|os.O_CREATE, 0644)
+	defer configFile.Close()
+	configFile.Write([]byte("[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = true\n"))
+
+	// write to description file
+	descriptionFile, _ := os.OpenFile(path.Join(".git", "description"), os.O_RDWR|os.O_CREATE, 0644)
+	defer descriptionFile.Close()
+	descriptionFile.Write([]byte("Unnamed repository; edit this file 'description' to name the repository.\n"))
 
 }
 
